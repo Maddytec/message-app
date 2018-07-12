a17aaf2677e3eccee6546a6948c78142b3ffc2e7
madarasilva <madarasilva@tjba.jus.br>
2018-07-12 13:38:08 -0300

    Add count like

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -12,29 +12,39 @@ export class FeedComponent implements OnInit {
       user: "keren",
       name: "Keren Apuque",
       body: "Keren is a library scientist living in New York City. She likes to spend her time reading, running, and writing.",
-      likes: "10000",
+      likes: [],
       avatar: "../assets/images/avatar/keren.jpg"
     },
     {
       user: "rosana",
       name: "Rosana Passos de Jesus Silva",
       body: "Rosana is a library scientist living in New York City. She likes to spend her time reading, running, and writing.",
-      likes: "2000",
+      likes: [],
       avatar: "../assets/images/avatar/rosana.jpg"
     },
     {
       user: "gabriel",
       name: "Gabriel Sena",
       body: "Gabriel is a library scientist living in New York City. She likes to spend her time reading, running, and writing.",
-      likes: "999",
+      likes: ['rosana', 'gabriel'],
       avatar: "../assets/images/avatar/gabriel.jpg"
     }
 
   ];
 
+  likedTweet = undefined;
+  currentUser = 'rosana';
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  handleLikedTweetFromChildComponent(tweet){
+    var index = this.tweets.findIndex(currTweet => {
+      return currTweet.user == tweet.user;
+    })
+    this.tweets[index].likes.push(this.currentUser);
   }
 
 }
